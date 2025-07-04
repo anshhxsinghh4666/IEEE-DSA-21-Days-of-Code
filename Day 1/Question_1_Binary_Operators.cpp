3684035b7396497087cc304ca5bfd363c7d0c218
Ansh Kumar Singh <anshkr.win8@gmail.com>
2025-07-04 14:59:09 +0530

    Start of IEEE DSA 21 days of code Day 1

---
+++ Day 1/Question_1_Binary_Operators.cpp
@@ -0,0 +1,39 @@
+/*
+Binary Operators
+Problem Statement:
+Create a program that calculates the bitwise AND, OR, XOR, and NOT operations on two integers provided by the user.
+
+Description:
+The program should read two integers and then compute and display the results of the bitwise AND, OR, XOR, and NOT operations.
+
+Input Format:
+
+Two integers separated by a space.
+Output Format:
+
+Four lines, each representing the result of bitwise AND, OR, XOR, and NOT operations respectively.
+
+Sample Input:
+10 4
+Sample Output:
+
+Bitwise AND: 0
+Bitwise OR: 14
+Bitwise XOR: 14
+Bitwise NOT (first number): -11
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << "Bitwise AND: " << (a & b) << endl;
+    cout << "Bitwise OR: " << (a | b) << endl;
+    cout << "Bitwise XOR: " << (a ^ b) << endl;
+    cout << "Bitwise NOT (first number): " << (~a) << endl;
+
+    return 0;
+}
